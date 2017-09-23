c3c5beab01abf518d5c86dacd7f5fa8fb27c0b88
yfang1644 <yfang1644@gmail.com>
2017-09-24 00:20:12 +0800

    rrmj get token

--- plugin.video.rrmj/resources/lib/rrmj.py
+++ plugin.video.rrmj/resources/lib/rrmj.py
@@ -9,10 +9,12 @@ from common import get_html
 import xbmcvfs
 import xbmcgui
 import xbmcaddon
+from random import randrange
+
 SERVER = "https://api.rr.tv"
 __ADDON__ = xbmcaddon.Addon()
 
-TOKEN = 'e4c636f879274746a4ae3a544a6926d2'
+SECRET_KEY = "clientSecret=08a30ffbc8004c9a916110683aab0060"
 
 FAKE_HEADERS = {
     "a": "cf2ecd4d-dea3-40ca-814f-3f0462623b1c",
@@ -22,7 +24,7 @@ FAKE_HEADERS = {
     "c": "5a1fb134-9384-4fc8-a5ae-6e711e24afc1",
     "d": "",
     "e": "d4dd075d894dd2b8c81f96062dbe7dcbf7d467fd",
-    "token": TOKEN,
+    "token": 'e4c636f879274746a4ae3a544a6926d2'
 }
 
 
@@ -62,14 +64,16 @@ class RenRenMeiJu(object):
 
     def __init__(self):
         self._header = FAKE_HEADERS
+        self.get_token()
+        self._header['token'] = FAKE_HEADERS['token']
         key_id = getGUID()
         self._header.update(a=key_id)
         self.get_ticket()
 
-    def get_json(self, url, data=None, pretty=False):
+    def get_json(self, api, data=None, pretty=False):
         headers = self.header
-        headers.update(b=url)
-        s = json.loads(get_html(url, data=data, headers=headers))
+        headers.update(b=SERVER+api)
+        s = json.loads(get_html(SERVER+api, data=data, headers=headers))
         if pretty:
             print headers
             print json.dumps(s, sort_keys=True,
@@ -85,7 +89,7 @@ class RenRenMeiJu(object):
         API = '/auth/ticket'
         auth_data = {"a": FAKE_HEADERS["a"],
                      "b": createKey()}
-        data = self.get_json(SERVER + API, data=urllib.urlencode(auth_data))
+        data = self.get_json(API, data=urllib.urlencode(auth_data))
         if data["data"]["ticket"] != "":
             __ADDON__.setSetting("expired_time", str(
                 data["data"]["expiredTime"]))
@@ -99,25 +103,46 @@ class RenRenMeiJu(object):
         API = '/v3plus/video/search'
         kwargs["page"] = page
         kwargs["rows"] = rows
-        return self.get_json(SERVER + API, data=urllib.urlencode(kwargs))
+        return self.get_json(API, data=urllib.urlencode(kwargs))
 
     def get_album(self, albumId=2):
         API = '/v3plus/video/album'
-        return self.get_json(SERVER + API, data=urllib.urlencode(dict(albumId=albumId)))
+        return self.get_json(API, data=urllib.urlencode(dict(albumId=albumId)))
 
     def index_info(self):
         API = '/v3plus/video/indexInfo'
-        return self.get_json(SERVER + API)
+        return self.get_json(API)
 
     def video_detail(self, seasonId, userId=0, **kwargs):
         API = '/v3plus/season/detail'
         kwargs["seasonId"] = seasonId
-        kwargs["token"] = TOKEN
-        return self.get_json(SERVER + API, data=urllib.urlencode(kwargs))
+        kwargs["token"] = self._header['token']
+        return self.get_json(API, data=urllib.urlencode(kwargs))
 
     def hot_word(self):
         API = '/v3plus/video/hotWord'
-        return self.get_json(SERVER + API)
+        return self.get_json(API)
+
+    def get_token(self):
+        API = '/user/platReg'
+        dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        dic += "abcdefghijklmnopqrstuvwxyz0123456789"
+        name = ''
+        for x in range(0,8):
+            name += dic[randrange(62)]
+
+        MD5 = hashlib.md5()
+        MD5.update(name)
+
+        body = {
+            'usid': MD5.hexdigest(),
+            'platformName': 'qq',
+            'nickName': name,
+            'userName': name,
+            'securityCode': ''
+        }
+        data = self.get_json(API, data=urllib.urlencode(body))
+        FAKE_HEADERS['token']= data['data']['user']['token']
 
 
 class RRMJResolver(RenRenMeiJu):
@@ -128,8 +153,9 @@ class RRMJResolver(RenRenMeiJu):
         headers = self.header
         l2 = str(int(time.time()) * 1000 - 28800000)
         headers['t'] = l2
-        s7 = "episodeSid=%squality=%sclientType=%sclientVersion=%st=%sclientSecret=08a30ffbc8004c9a916110683aab0060" % (
-            episodeSid, quality, headers['clientType'], headers['clientVersion'], l2)
+        s7 = "episodeSid=%squality=%sclientType=%sclientVersion=%st=%s%s" % (
+            episodeSid, quality, headers['clientType'],
+            headers['clientVersion'], l2, SECRET_KEY)
         MD5 = hashlib.md5()
         MD5.update(s7)
         signature = MD5.hexdigest()
